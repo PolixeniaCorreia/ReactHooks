e187a0c240834f922cccb35caea78a5e14eb6e4f
Polixenia Correia <polixeniacorreia@gmail.com>
2020-07-14 11:41:31 -0300

    Usando Classe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    valor: 1
+  }
+  componentDidMount(){
+    setInterval(() => {
+      this.setState({ valor: this.state.valor + 1})
+    }, 1000)
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Valor: {this.state.valor}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
